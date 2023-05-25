8b5f8028a9195eb81ba68abc52a51f5ac4367513
chaneldevelops <chanel.developsdesigns@gmail.com>
2023-05-25 04:35:19 -0400

    Added necessary imports to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Info from "./components/Info";
+import About from "./components/About"
+import Interests from "./components/Interest"
+import Footer from "./components/Footer"
 
 export default function App() {
 return (
