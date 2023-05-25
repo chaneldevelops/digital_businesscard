8515d1350b2ec50e29662ce0e4629a7bc210bf16
chaneldevelops <chanel.developsdesigns@gmail.com>
2023-05-25 04:30:02 -0400

    Vite setup Files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Info from "./components/Info";
+
+export default function App() {
+return (
+        <div>Hello World</div>
+)
+
+        
+
+}
---
+++ src/components/Info.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+// import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+// import { faLinkedin } from "@fortawesome/free-brands-svg-icons";
+
+
+
+
+export default function Info() {
+    <div>
+        <img src="" alt="" />
+        <h1>Chanel Waters</h1>
+        <h5>Front-End Developer</h5>
+        
+        <div className="contact-btns">
+            <button>
+            {/* <FontAwesomeIcon icon="fa-solid fa-envelope" /> */}
+                 Email<a href="mailto:chanel.developsdesigns@gmail.com">
+                </a>
+            </button>
+
+            <button>
+            {/* <FontAwesomeIcon icon="fa-brands fa-linkedin" /> */}
+                 Connect<a href="#">
+                </a>
+            </button>
+        </div>
+    </div>
+}
