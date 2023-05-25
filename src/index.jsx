074f02d5c86d268bad48c188cb404e34d98f6629
chaneldevelops <chanel.developsdesigns@gmail.com>
2023-05-25 06:02:55 -0400

    Starter styles
    - added custom props

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import ReactDOM from 'react-dom/client' // this is the new addition from the scrimba
 import App from './App.jsx'
+import "../src/style.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
